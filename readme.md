43919505118f0cc428ba04a333cd1999b53ab41c
Egill Hreinsson <egillh210@gmail.com>
2019-05-23 06:56:19 +0200

    init commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+## Hoover Robot
+
+# info
+There are two copies of the script in this repo. One with comments, and one without.
+To run script agains a specific .txt file, run "node main.js [pathoftextfile]".
+
+# setup 
+clone the repo
+- npm install
+- npm test
--- test/assertiontest.js
+++ test/assertiontest.js
@@ -12,7 +12,7 @@ fs.readFile(filename1, 'utf8', function(err, data) {
   const assertion = 'It should remove patches and count them only once: ';
   data = data.split('\n').filter(str => str.length > 0);
   data.pop();
-  data[0] == input1[0] && data[1] == input1[1] ? console.log(assertion.yellow + 'true'.green) : console.log(assertion.yellow + 'false'.red);
+  data[0] == input1[0] && data[1] == input1[1] ? console.log(assertion.yellow + '✅') : console.log(assertion.yellow + '❌');
 })
 
 fs.readFile(filename2, 'utf8', function(err, data) {
@@ -20,7 +20,7 @@ fs.readFile(filename2, 'utf8', function(err, data) {
   const assertion = 'It should stay in place if direction is invalid: ';
   data = data.split('\n').filter(str => str.length > 0);
   data.pop();
-  data[0] == input2[0] && data[1] == input2[1] ? console.log(assertion.yellow + 'true'.green) : console.log(assertion.yellow + 'false'.red);
+  data[0] == input2[0] && data[1] == input2[1] ? console.log(assertion.yellow + '✅') : console.log(assertion.yellow + '❌');
 })
 
 fs.readFile(filename3, 'utf8', function(err, data) {
@@ -28,6 +28,6 @@ fs.readFile(filename3, 'utf8', function(err, data) {
   const assertion = 'It should default to nearest valid square if starting point is invalid: ';
   data = data.split('\n').filter(str => str.length > 0);
   data.pop();
-  data[0] == input3[0] && data[1] == input3[1] ? console.log(assertion.yellow + 'true'.green) : console.log(assertion.yellow + 'false'.red);
+  data[0] == input3[0] && data[1] == input3[1] ? console.log(assertion.yellow + '✅') : console.log(assertion.yellow + '❌');
 })
 
